0843a1399bc7926840b4511cc99b1987e078c351
dashBuresz <csaba.szijarto03@gmail.com>
2025-02-09 16:21:24 +0100

    Javascript basics completed

--- project-1-website/index.js
+++ project-1-website/index.js
@@ -1,1 +1,11 @@
 //The JavaScript file is in charge of the interactivity of our website (Actions)
+//prints to the devtools console
+//console.log("Hello");
+//console.log("I like pizza");    
+
+//window.alert("This is an alert"); //creates an alert on the website
+//window.alert("I like pizza");
+
+// We're accessing the header variable from the html document ans assigning it a value here
+document.getElementById("MyH1").textContent = "Hello"; 
+document.getElementById("myP").textContent = "I like pizza";
